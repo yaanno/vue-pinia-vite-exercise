54455ad66c7f21a6b31c5100d990be65d3857d78
Janos Hardi <janos.hardi@gmail.com>
2023-04-29 11:11:29 +0200

    if the article is not found

--- src/views/ArticlePage.vue
+++ src/views/ArticlePage.vue
@@ -10,13 +10,21 @@ const article = store.articles.find(article => article.slug === route.params.slu
 </script>
 
 <template>
-	<h1>{{ article?.title }}</h1>
-	<p>{{ dateToLocaleString(article!.published_at) }}</p>
-	<div class="article_content">
-      <p>{{ article?.excerpt }}</p>
-    </div>
-	<div class="article_tags">
-      <ArticleTag :tag="tagValue" type="tags" v-for="tagValue in article?.tags" />
-      <ArticleTag :tag="placeValue" type="places" v-for="placeValue in article?.places" />
-    </div>
+	<div v-if="article">
+		<h1>{{ article.title }}</h1>
+		<p>{{ dateToLocaleString(article.published_at) }}</p>
+		<div class="article_illustration">
+        <img src="https://picsum.photos/600/150" alt="Illustration">
+      </div>
+		<div class="article_content">
+			<p>{{ article.excerpt }}</p>
+		</div>
+		<div class="article_tags">
+			<ArticleTag :tag="tagValue" type="tags" v-for="tagValue in article.tags" />
+			<ArticleTag :tag="placeValue" type="places" v-for="placeValue in article.places" />
+		</div>
+	</div>
+	<div v-else>
+		<h1>Article not found ;_;</h1>
+	</div>
 </template>
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,31 +1,9 @@
 <script setup lang="ts">
-// import articles from "../data/articles.json";
 import Article from "../components/Article.vue";
 import Search from "../components/Search.vue";
 import { useDefaultStore } from "../stores/defaultStore";
 
-// const sortedArticles = articles.sort(
-//   (a, b) =>
-//     new Date(a.published_at).getTime() -
-//     new Date(b.published_at).getTime()
-// )
-// const tags = sortedArticles.flatMap(article => article.tags);
-// const filteredTags = [... new Set(tags)];
-
-// const places = sortedArticles.flatMap(article => article.places).filter(p => p);
-// const filteredPlaces = [... new Set(places)];
-
 const store = useDefaultStore();
-
-// store.$patch({
-//   articles: sortedArticles,
-//   tags: filteredTags,
-//   places: filteredPlaces,
-// })
-
-// store.$subscribe(() => {
-//   store.filterArticles()
-// })
 
 </script>
 
